e34fc2104738ae630baa452f6abd5b1e67074981
mrohling13 <malachi.rohling@yahoo.com>
2025-01-26 16:14:51 -0500

    Task 1 - Employee Information

---
+++ Coding_Challenge#1/cc_1.js
@@ -0,0 +1,8 @@
+// Task 1: Variables
+let employeeName = "John Doe";
+const employeeID = 12345;
+var isActive = true;
+
+console.log(employeeName, typeof employeeName); // John Doe string
+console.log(employeeID, typeof employeeID);     // 12345 number
+console.log(isActive, typeof isActive);         // true boolean
